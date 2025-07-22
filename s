9db05bccc8b698017c073e9bcdd8457ccc2eb103
vippergod12 <dangnhattien1101@gmail.com>
2025-07-22 20:25:04 +0700

    Commit01: Tien - update Product image and API get Image

---
+++ s
@@ -0,0 +1,11 @@
+  feat_ApplicationInitConfig[m
+  feat_DayOfBirth[m
+  feat_UpdateUser[m
+  feat_backup[m
+  feat_logout[m
+  feat_security[m
+  feat_securityConfigUpdate[m
+  feat_updateGetUsersWithAuthority[m
+  feat_userprofile[m
+* [32mmain[m
+  role_permission[m
--- src/main/java/ws/prj/config/WebConfig.java
+++ src/main/java/ws/prj/config/WebConfig.java
@@ -1,6 +1,7 @@
 package ws.prj.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,6 +12,13 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:" + System.getProperty("user.dir") + "/uploads/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // hoặc cụ thể như "/users"
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("*");
     }
 }
 
