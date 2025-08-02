a3608872c836a8c22eb7b122809b9fd329b463ec
Quang <minhquangru@gmail.com>
2025-08-02 08:51:24 +0700

    pull

--- src/main/java/ws/prj/controller/AuthenticationController.java
+++ src/main/java/ws/prj/controller/AuthenticationController.java
@@ -20,7 +20,7 @@ import java.text.ParseException;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE)
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AuthenticationController {
 
     AuthenticationService authService;
